f4b8a792c214043f16e22e490e6fbfc71b50bb29
akunku767 <mrifqi767@gmail.com>
2025-07-05 20:16:38 +0700

    a

--- resources/views/admin/layout/pdf/TestResult.blade.php
+++ resources/views/admin/layout/pdf/TestResult.blade.php
@@ -22,15 +22,6 @@
             align-items: center;
         }
 
-        .card-container {
-            width: 90%;
-            max-width: 600px;
-            padding: 20px;
-            text-align: center;
-            border-radius: 5px;
-            margin: 50px 0 0 20px;
-        }
-
         h2 {
             margin-top: 0;
             font-size: 24px;
@@ -42,24 +33,6 @@
             line-height: 1.5;
             color: #333;
             text-align: justify;
-        }
-
-        .button {
-            display: inline-block;
-            margin-bottom: 1rem;
-            margin-top: 1rem;
-            padding: 10px 20px;
-            font-size: 16px;
-            color: #fff;
-            background-color: #e60012;
-            border: none;
-            border-radius: 5px;
-            text-decoration: none;
-            cursor: pointer;
-        }
-
-        .button:hover {
-            background-color: #96000d;
         }
 
         .bold {
@@ -108,116 +81,112 @@
 </head>
 
 <body>
-    <div class="card-container">
-
-        <div class="header">
-            <img src="https://remision.my.id/assets/img/Icon_Remision.png" alt="Image" width="auto"
-                style="height: 4.5rem; margin: 0px 5px">
-            <div class="IsiJudul">
-                <div class="bold">"Berkah Auto Garage" </div>
-                <div class="bold">"REMISION" </div>
-                Bengkel Mobil dan Reparasi
-                Jalan Baru Jadi No. 103, Kota Surabaya, Jawa Timur<br>
-                Tel/Fax: 031-673924212, e-mail: info@remision.my.id
-            </div>
-        </div>
-
-        <div class="line"></div>
-
-        <center>
-            <span>
-                <b>
-                    SERTIFIKAT UJI EMISI
-                </b>
-            </span>
-        </center>
-
-        <div style="text-align: right;">
-            Jakarta, 25 Juni 2019
-        </div>
-
-        <table style="margin-top: 10px;">
-            <tr>
-                <td>No</td>
-                <td>: {{ $number }}</td>
-            </tr>
-            <tr>
-                <td>Lamp</td>
-                <td>: -</td>
-            </tr>
-            <tr>
-                <td>Hal</td>
-                <td>: Laporan Uji Emisi</td>
-            </tr>
-        </table>
-
-        <p style="margin-top:3rem; margin-bottom: 0.5rem;">
-            Yth. Kepala Dinas Perhubungan,<br>
-            Kab. Sidoarjo<br>
-            di Tempat<br><br>
-
-            Sehubungan dengan telah diberlakukannya PERMEN LHK REPUBLIK INDONESIA NOMOR 8 TAHUN 2023 TENTANG
-            PENERAPAN BAKU MUTU EMISI KENDARAAN BERMOTOR KATEGORI M, KATEGORI N, KATEGORI O, DAN KATEGORI L.
-            Dengan ini menerangkan bahwa: <br>
-        </p>
-        <table style="border: 0px">
-            <tbody>
-                <tr>
-                    <td>No Kendaraan</td>
-                    <td>: {{ $license_plate }}</td>
-                </tr>
-                <tr>
-                    <td>Merk</td>
-                    <td>: {{ $brand }}</td>
-                </tr>
-                <tr>
-                    <td>Tahun Produksi</td>
-                    <td>: {{ $year }}</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <p style="margin-top:2rem; margin-bottom: 0.5rem;">
-            Telah mengikuti uji emisi pada {{ $date }}di Pelaksana Uji Emisi "REMISION", kendaraan
-            bermotor tersebut memenuhi ambang batas emisi gas buang dan dinyatakan
-            <b>{{ $result }}</b> dengan indeks emisi:
-        </p>
-        <table>
-            <thead>
-                <tr>
-                    <th style="text-align: center; border: 2px solid">
-                        O2
-                    </th>
-                    <th style="text-align: center; border: 2px solid">
-                        CO2
-                    </th>
-                    <th style="text-align: center; border: 2px solid">
-                        CO
-                    </th>
-                    <th style="text-align: center; border: 2px solid">
-                        HC
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $O2 }}</td>
-                    <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $CO2 }}</td>
-                    <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $CO }}</td>
-                    <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $HC }}</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <div class="footer" style="margin-top: 2rem">
-            <p style="margin: 0px !important;">Copyright &copy; {{ date('Y') }} REMISION (Reduce Emision with IoT
-                Solution). All rights reserved.</p>
-            <p style="margin: 0px !important;">Untuk informasi lebih lanjut, kunjungi <a
-                    href="https://remision.my.id/">website kami</a>.</p>
+    <div class="header">
+        <img src="https://remision.my.id/assets/img/Icon_Remision.png" alt="Image" width="auto"
+            style="height: 4.5rem; margin: 0px 5px">
+        <div class="IsiJudul">
+            <div class="bold">BERKAH AUTO GARAGE</div>
+            <div class="bold">"REMISION" </div>
+            Bengkel Mobil dan Reparasi<br>
+            Jalan Baru Jadi No. 103, Kota Surabaya, Jawa Timur<br>
+            Tel/Fax: 031-673924212, e-mail: info@remision.my.id
         </div>
     </div>
 
+    <div class="line"></div>
 
+    <center>
+        <span>
+            <b>
+                SERTIFIKAT UJI EMISI
+                <br>
+            </b>
+        </span>
+    </center>
+
+    <div style="text-align: right;">
+        Jakarta, 25 Juni 2019
+    </div>
+
+    <table style="margin-top: 10px;">
+        <tr>
+            <td>No</td>
+            <td>: {{ $number }}</td>
+        </tr>
+        <tr>
+            <td>Lamp</td>
+            <td>: -</td>
+        </tr>
+        <tr>
+            <td>Hal</td>
+            <td>: Laporan Uji Emisi</td>
+        </tr>
+    </table>
+
+    <p style="margin-top:3rem; margin-bottom: 0.5rem;">
+        Yth. Kepala Dinas Perhubungan,<br>
+        Kab. Sidoarjo<br>
+        di Tempat<br><br>
+
+        Sehubungan dengan telah diberlakukannya PERMEN LHK REPUBLIK INDONESIA NOMOR 8 TAHUN 2023 TENTANG
+        PENERAPAN BAKU MUTU EMISI KENDARAAN BERMOTOR KATEGORI M, KATEGORI N, KATEGORI O, DAN KATEGORI L.
+        Dengan ini menerangkan bahwa: <br>
+    </p>
+    <table style="border: 0px">
+        <tbody>
+            <tr>
+                <td>No Kendaraan</td>
+                <td>: {{ $license_plate }}</td>
+            </tr>
+            <tr>
+                <td>Merk</td>
+                <td>: {{ $brand }}</td>
+            </tr>
+            <tr>
+                <td>Tahun Produksi</td>
+                <td>: {{ $year }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <p style="margin-top:2rem; margin-bottom: 0.5rem;">
+        Telah mengikuti uji emisi pada {{ $date }}di Pelaksana Uji Emisi "REMISION", kendaraan
+        bermotor tersebut memenuhi ambang batas emisi gas buang dan dinyatakan
+        <b>{{ $result }}</b> dengan indeks emisi:
+    </p>
+    <table>
+        <thead>
+            <tr>
+                <th style="text-align: center; border: 2px solid">
+                    O2
+                </th>
+                <th style="text-align: center; border: 2px solid">
+                    CO2
+                </th>
+                <th style="text-align: center; border: 2px solid">
+                    CO
+                </th>
+                <th style="text-align: center; border: 2px solid">
+                    HC
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $O2 }}</td>
+                <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $CO2 }}</td>
+                <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $CO }}</td>
+                <td width="25%" style="text-align: center; border: 2px solid #fff">{{ $HC }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <div class="footer" style="margin-top: 2rem">
+        <p style="margin: 0px !important;">Copyright &copy; {{ date('Y') }} REMISION (Reduce Emision with IoT
+            Solution). All rights reserved.</p>
+        <p style="margin: 0px !important;">Untuk informasi lebih lanjut, kunjungi <a
+                href="https://remision.my.id/">website kami</a>.</p>
+    </div>
 </body>
 
 </html>
