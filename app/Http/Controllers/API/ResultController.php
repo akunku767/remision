adf95ef1fe57bc9f090c9f121fb8d23677ec631f
akunku767 <mrifqi767@gmail.com>
2025-07-05 23:35:40 +0700

    a

--- app/Http/Controllers/API/ResultController.php
+++ app/Http/Controllers/API/ResultController.php
@@ -32,84 +32,10 @@ class ResultController extends BaseController
     /**
      * Show the form for creating a new resource.
      */
-    public function exCreate(Request $request)
-    {
-        $reff_num = mt_rand(1000000000, 9999999999);
-        $create = Result::create([
-            'vehicle_id' => $request->vehicle_id,
-            'tested_at' => $request->tested_at,
-            'O2' => $request->O2,
-            'CO2' => $request->CO2,
-            'CO' => $request->CO,
-            'HC' => $request->HC,
-            'reference_number' => $reff_num,
-            'identity' => Str::random(10),
-        ]);
-
-        if ($create) {
-            $vehicle = $create->vehicle;
-            $user = $vehicle->user;
-            $CO = $vehicle->threshold->CO;
-            $HC = $vehicle->threshold->HC;
-
-            if ($request->CO <= $CO && $request->HC <= $HC) {
-                $data = [
-                    'license_plate' => $vehicle->license_plate,
-                    'O2' => $request->O2 . ' %',
-                    'CO2' => $request->CO2 . ' %',
-                    'CO' => $request->CO . ' %',
-                    'HC' => $request->HC . ' ppm',
-                    'brand' => $vehicle->brand,
-                    'result' => 'LULUS',
-                    'date' => $request->tested_at,
-                    'number' => "" . Carbon::now()->format('Y/m/') . "EMISI/II" . Carbon::now()->format('/d') . "/" . $reff_num,
-                ];
-            } else {
-                $data = [
-                    'license_plate' => $vehicle->license_plate,
-                    'O2' => $request->O2 . ' %',
-                    'CO2' => $request->CO2 . ' %',
-                    'CO' => $request->CO . ' %',
-                    'HC' => $request->HC . ' ppm',
-                    'brand' => $vehicle->brand,
-                    'result' => 'TIDAK LULUS',
-                    'date' => $request->tested_at,
-                    'number' => "" . Carbon::now()->format('Y/m/') . "EMISI/II" . Carbon::now()->format('/d') . "/" . $reff_num,
-                ];
-            }
-
-            // Generate PDF dari view
-            $pdf = Pdf::loadView('admin.layout.pdf.TestResult', $data)
-                ->setPaper('A4', 'portrait') // Sesuaikan ukuran dan orientasi kertas
-                ->setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);
-
-            // Menyimpan PDF ke file
-            $pdfFileName = 'TestResult_' . Str::random(10) . '.pdf';
-            $pdf->save(storage_path('app/public/' . $pdfFileName));
-
-            // Kirim email dengan lampiran PDF
-            Mail::send([], [], function ($message) use ($data, $user, $pdfFileName) {
-                $message->to($user->email)
-                    ->subject('Hasil Uji Emisi')
-                    ->html(view('admin.layout.mail.TestResult', $data)->render())
-                    ->attach(storage_path('app/public/' . $pdfFileName), [
-                        'as' => $pdfFileName,
-                        'mime' => 'application/pdf'
-                    ]);
-            });
-
-            // Opsional: Hapus file PDF setelah email terkirim
-            unlink(storage_path('app/public/' . $pdfFileName));
-
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Success added data',
-            ]);
-        }
-    }
 
     public function create(Request $request)
     {
+        Carbon::setLocale('id');
         $tested_at = Carbon::now();
         $reff_num = mt_rand(1000000000, 9999999999);
         $create = Result::create([
